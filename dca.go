698385642617db5a82f9f8ff1eb8d27b546bf975
David Hollinger <david.hollinger@gmail.com>
2022-03-29 08:58:56 -0500

    test

--- dca.go
+++ dca.go
@@ -4,6 +4,6 @@ import "fmt"
 
 // first test of package and github sync
 func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf("Hi loser, %v. Welcome!", name)
 	return message
 }
