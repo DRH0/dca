7b5e47a840e2f9b817ffb8c6148be49c7f76e032
David Hollinger <david.hollinger@gmail.com>
2022-07-05 11:27:02 -0500

    changes

--- dca_test.go
+++ dca_test.go
@@ -1,26 +1,46 @@
 package dca
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestHyperbolic(t *testing.T) {
-	type args struct {
-		result [][]float64
+func Make2df64(rows int, cols int) [][]float64 {
+	table := make([][]float64, rows)
+	chunk := make([]float64, cols*rows)
+	for i := range table {
+		table[i], chunk = chunk[:cols], chunk[cols:]
+	}
+	return table
+}
+func Print2df64(table [][]float64) {
+	for _, row := range table {
+		for _, col := range row {
+			fmt.Print(fmt.Sprintf("%.2f", col) + " ")
+		}
+		fmt.Print("\n")
+	}
+}
+func TestDeclineCurve(t *testing.T) {
+
+	var tests = []struct {
+		name   string
 		Qi     float64
 		Di     float64
 		n      float64
 		Dt     float64
 		length int
 		delay  int32
-	}
-	tests := []struct {
-		name string
-		args args
+		result [][]float64
 	}{
-		// TODO: Add test cases.
+		{"Test 1", 25000, 80, 1.2, 7, 10, 0, Make2df64(10, 7)},
+		{"Test 2", 25000, 99.9999, 0, 7, 10, 0, Make2df64(10, 7)},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			Hyperbolic(tt.args.result, tt.args.Qi, tt.args.Di, tt.args.n, tt.args.Dt, tt.args.length, tt.args.delay)
+			DeclineCurve(tt.result, tt.Qi, tt.Di, tt.n, tt.Dt, tt.length, tt.delay)
+			//Print2df64(tt.result)
 		})
 	}
 }
